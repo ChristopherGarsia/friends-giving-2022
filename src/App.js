0d1bda2a443c0ccf53775baf1b4ec7b598484ee8
christophergarsia <christopherjgarsia@gmail.com>
2021-11-09 00:21:07 -0800

    Construction Page

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
+import lastSupper from './LastSupper.png';
+import PhoneInput from 'react-phone-input-2'
+import 'react-phone-input-2/lib/style.css'
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Friends Giving 2021 Tahoe Edition!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <img src={lastSupper} class="App-logo" alt="logo" />
+        <p>
+          This site is currently under construction...
+        </p>
+        <p>
+          Please Enter your Mobile Number to receive Text Updates for Friendsgiving
+        </p>
+        <div>
+        <PhoneInput
+          onlyCountries={['us']}
+          country={'us'}
+        />
+        <input type="submit" value="Submit"/>
+        </div>
       </header>
     </div>
   );
